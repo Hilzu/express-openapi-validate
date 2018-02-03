d3892dbe084aa798a3425bc553caf98322576504
Santeri Hiltunen <accounts.github@me.hilzu.moe>
2018-02-03 18:04:20 +0200

    Initialize statusCode field in definition

--- src/ValidationError.ts
+++ src/ValidationError.ts
@@ -17,13 +17,12 @@
 import { ErrorObject } from "ajv";
 
 export default class ValidationError extends Error {
-  public statusCode: number;
+  public statusCode: number = 400;
   public data: ErrorObject[];
 
   constructor(message: string, errors: ErrorObject[]) {
     super(message);
     this.name = this.constructor.name;
-    this.statusCode = 400;
     this.data = errors;
   }
 }
