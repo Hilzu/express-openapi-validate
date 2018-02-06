46cc0e3e4dc4c20bb344ab2efc517c3a20053bb1
Santeri Hiltunen <accounts.github@me.hilzu.moe>
2018-02-06 20:34:22 +0200

    Add RunKit example file and badge

---
+++ resources/runkit-example.js
@@ -0,0 +1,50 @@
+const jsYaml = require("js-yaml");
+const {
+  OpenApiValidator,
+  ValidationError,
+} = require("express-openapi-validate");
+
+const openApiDocument = jsYaml.safeLoad(`
+openapi: 3.0.1
+info:
+  title: Test API
+  version: 1.0.0
+paths:
+  /echo:
+    post:
+      requestBody:
+        content:
+          application/json:
+            schema:
+              type: object
+              properties:
+                input:
+                  type: string
+              required:
+                - input
+        required: true
+      responses:
+        200:
+          description: Echoed input
+`);
+
+const validator = new OpenApiValidator(openApiDocument);
+// This is the express middleware that does the validation
+const validate = validator.validate("post", "/echo");
+const fakeRequest = {
+  body: {},
+  query: {},
+  headers: {},
+  cookies: {},
+  params: {},
+};
+const fakeResponse = {};
+const nextFunction = err => {
+  console.log("Error passed to next:", err);
+};
+
+validate(fakeRequest, fakeResponse, nextFunction);
+const requestWithBody = Object.assign({}, fakeRequest, {
+  body: { input: "Hello!" },
+});
+validate(requestWithBody, fakeResponse, nextFunction);
