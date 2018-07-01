b00e403a354df0642e903f70a0bbc9acb572b0e7
Santeri Hiltunen <accounts.github@me.hilzu.moe>
2018-07-01 20:27:00 +0300

    Extract eslint cofiguration from package.json

---
+++ .eslintrc.js
@@ -0,0 +1,39 @@
+module.exports = {
+  extends: ["eslint:recommended", "airbnb-base", "prettier"],
+  parser: "typescript-eslint-parser",
+  settings: {
+    "import/extensions": [".ts", ".js"],
+    "import/resolver": {
+      node: {
+        extensions: [".ts", ".js", ".json"],
+      },
+    },
+  },
+  env: {
+    jest: true,
+    node: true,
+  },
+  rules: {
+    "import/extensions": [
+      "error",
+      "always",
+      {
+        ts: "never",
+        js: "never",
+      },
+    ],
+    "import/no-unresolved": [
+      "error",
+      {
+        ignore: ["\\.\\.?/dist/"],
+      },
+    ],
+    "linebreak-style": "off",
+    "no-restricted-globals": "off",
+    "no-undef": "off",
+    "no-underscore-dangle": "off",
+    "no-unused-vars": "off",
+    "no-useless-constructor": "off",
+    strict: "off",
+  },
+};
